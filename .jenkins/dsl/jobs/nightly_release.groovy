b36a3b8bf8fdb0d8f5bdbf060207ed11f63f0432
radtriste <tradisso@redhat.com>
2020-12-21 17:15:44 +0100

    GIT_BRANCH_NAME

--- .jenkins/dsl/jobs/nightly_release.groovy
+++ .jenkins/dsl/jobs/nightly_release.groovy
@@ -69,7 +69,7 @@ KogitoJobTemplate.createPipelineJob(this, getJobParams('kogito-operator-deploy',
     environmentVariables {
         env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
 
-        env('GIT_BRANCH', "${GIT_BRANCH}")
+        env('GIT_BRANCH_NAME', "${GIT_BRANCH}")
         env('GIT_AUTHOR', "${GIT_AUTHOR_NAME}")
         env('GIT_AUTHOR_CREDS_ID', "${GIT_AUTHOR_CREDENTIALS_ID}")
         env('GITHUB_TOKEN_CREDS_ID', "${GIT_AUTHOR_TOKEN_CREDENTIALS_ID}")
@@ -115,7 +115,7 @@ KogitoJobTemplate.createPipelineJob(this, getJobParams('kogito-operator-promote'
     environmentVariables {
         env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
 
-        env('GIT_BRANCH', "${GIT_BRANCH}")
+        env('GIT_BRANCH_NAME', "${GIT_BRANCH}")
         env('GIT_AUTHOR', "${GIT_AUTHOR_NAME}")
         env('GIT_AUTHOR_CREDS_ID', "${GIT_AUTHOR_CREDENTIALS_ID}")
         env('GITHUB_TOKEN_CREDS_ID', "${GIT_AUTHOR_TOKEN_CREDENTIALS_ID}")
